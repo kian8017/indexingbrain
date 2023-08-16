316f337b3a3da6745ecad2e1ab68d8e62d278914
Kian Musser <kianmusser@gmail.com>
2023-08-16 21:50:56 +0000

    test(name_search): get existing tests working

--- packages/name_search/locations.test.js
+++ packages/name_search/locations.test.js
@@ -2,9 +2,10 @@ import { describe, it, expect } from "@jest/globals";
 import { parseLocations } from "./locations";
 import { join } from "path";
 
+
 describe("locations", () => {
   it("should correctly parse the test folder", async () => {
-    const testFolder = join(__dirname, "test");
+    const testFolder = new URL("test", import.meta.url);
     const locations = await parseLocations(testFolder);
     expect(locations).toMatchSnapshot();
   });
