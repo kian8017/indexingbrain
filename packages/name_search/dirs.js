f25f6f3d1c1fa896675be27696c818174da1f473
Kian Musser <kianmusser@gmail.com>
2023-08-17 16:26:06 +0000

    feat(name_search): implement getEntryTypes and getLocations

--- packages/name_search/dirs.js
+++ packages/name_search/dirs.js
@@ -1,7 +1,37 @@
+import { readdir } from "fs/promises";
 import { NameSearchBase } from "./base";
 
 export class NameSearchDirs extends NameSearchBase {
-  constructor() {
-    throw("rootDir not specified");
+  #rootDir;
+  constructor(rootDir) {
+    super();
+    this.#rootDir = rootDir;
+  }
+
+  async init() {
+    if (this.#rootDir === undefined) {
+      throw new Error("rootDir not specified");
+    }
+  }
+
+  async getEntryTypes() {
+    return [
+      { name: "names", abbr: "N" },
+      { name: "places", abbr: "P" },
+      { name: "other", abbr: "O" },
+    ];
+  }
+
+  async getLocations() {
+    const fsEntries = await readdir(this.#rootDir, { withFileTypes: true });
+    return fsEntries
+      .filter((entry) => entry.isDirectory())
+      .map((entry) => {
+        const pieces = entry.name.split(" ");
+        return {
+          abbr: pieces[0],
+          name: pieces.slice(1).join(" "),
+        };
+      });
   }
 }
