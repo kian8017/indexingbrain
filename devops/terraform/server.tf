38ab7d326b08ab266f9ffacbffde343ae145c158
Kian Musser <kianmusser@granderprogramming.com>
2023-08-30 14:32:40 -0500

    devops: add initial server .tf and .tfvars

---
+++ devops/terraform/server.tf
@@ -0,0 +1,17 @@
+resource "digitalocean_droplet" "ib-server" {
+  image = "ubuntu-22-04-x64"
+  name = "ib-server"
+  region = "sfo3"
+  size = "s-1vcpu-512mb-10gb"
+  ssh_keys = [
+    data.digitalocean_ssh_key.terraform.id
+  ]
+
+  connection {
+    host = self.ipv4_address
+    user = "root"
+    type = "ssh"
+    private_key = file(var.pvt_key)
+    timeout = "2m"
+  }
+}
