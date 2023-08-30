737a1d3f1c838845862c884b0817dad77fed5189
Kian Musser <kianmusser@granderprogramming.com>
2023-08-30 16:44:42 -0500

    devops(terraform): remove unused connection block

--- devops/terraform/server.tf
+++ devops/terraform/server.tf
@@ -6,14 +6,6 @@ resource "digitalocean_droplet" "ib-server" {
   ssh_keys = [
     data.digitalocean_ssh_key.terraform.id
   ]
-
-  connection {
-    host = self.ipv4_address
-    user = "root"
-    type = "ssh"
-    private_key = file(var.pvt_key)
-    timeout = "2m"
-  }
 }
 
 resource "digitalocean_reserved_ip" "ib-main-ip" {
