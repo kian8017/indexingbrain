1d8855a293fd7816e0e7aa5eb3000e99471f1304
Kian Musser <kianmusser@granderprogramming.com>
2023-08-30 18:19:17 -0500

    devops(ansible): add script to run ansible playbook and gitignore auto-generated hosts.yml

---
+++ devops/ansible/run-playbook.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+fileThis=$(realpath $0)
+
+sScriptName=$(basename "$fileThis")
+
+if [ "$#" -ne 1 ]; then
+  exec 1>&2
+  echo "usage: $sScriptName <ansible.playbook>"
+  exit 1
+fi
+
+dirAnsible=$(dirname "$fileThis")
+
+fileInventory="$dirAnsible/hosts.yml"
+
+if [ ! -f "$fileInventory" ]; then
+  exec 1>&2
+  echo "error: '$fileInventory' doesn't exist"
+  echo "error:"
+  echo "error: please try rerunning the terraform configuration with 'terraform apply' to generate the correct 'hosts.yml'"
+  exit 1
+fi
+
+ansible-playbook --inventory="$fileInventory" "$@"
