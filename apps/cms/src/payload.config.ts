507dafdeb6c7aef1de9f3ef8ab91079d8cd5847e
Kian Musser <kianmusser@granderprogramming.com>
2023-09-02 10:43:29 -0500

    build(cms): remove dotenv dependency AND clean up config file

--- apps/cms/src/payload.config.ts
+++ apps/cms/src/payload.config.ts
@@ -1,6 +1,5 @@
 import { buildConfig } from "payload/config";
 import path from "path";
-// import Examples from './collections/Examples';
 import Users from "./collections/Users";
 import CarouselPairs from "./collections/CarouselPairs";
 import CouldBes from "./collections/CouldBes";
@@ -10,15 +9,6 @@ import Replacements from "./collections/Replacements";
 import Message from "./globals/Message";
 import Images from "./collections/Images";
 import FrequentlyAskedQuestions from "./collections/FrequentlyAskedQuestions";
-
-import dotenv from "dotenv";
-dotenv.config({
-  path: path.resolve(__dirname, '../.env'),
-});
-
-// GitHub Codespaces rewrites the port-forwarded requests, so this is needed,
-//     even though technically the app isn't being accessed at 'localhost:<PORT>'
-const secureLocalAddr = `https://localhost:${process.env.PAYLOAD_PUBLIC_PORT}`;
 
 export default buildConfig({
   serverURL: process.env.PAYLOAD_PUBLIC_ADDRESS,
@@ -35,7 +25,7 @@ export default buildConfig({
     Images,
     FrequentlyAskedQuestions,
   ],
-  csrf: [secureLocalAddr, ...process.env.PAYLOAD_PUBLIC_FRONTEND_ADDRESS.split(",")],
+  //csrf: [secureLocalAddr, ...process.env.PAYLOAD_PUBLIC_FRONTEND_ADDRESS.split(",")],
   globals: [Message],
   typescript: {
     outputFile: path.resolve(__dirname, "payload-types.ts"),
