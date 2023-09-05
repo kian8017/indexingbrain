3e05cd07165680039ee7d4499bc0781bd34d00dc
Kian Musser <kianmusser@granderprogramming.com>
2023-09-05 19:50:37 +0000

    test(faq-provider): update testing to use snapshot

--- packages/faq-provider/main.test.ts
+++ packages/faq-provider/main.test.ts
@@ -2,11 +2,22 @@ import { describe, it, expect } from "@jest/globals";
 import { PayloadFAQProvider } from "./main";
 
 // include protocol
-const PAYLOAD_ADDRESS="http://cms:3000";
+const PAYLOAD_ADDRESS = "http://cms:3000";
 
 describe("PayloadFAQProvider", () => {
-	it("should return the correct number of results with the seeded data", async () => {
-		const provider = new PayloadFAQProvider(PAYLOAD_ADDRESS);
-		await expect(provider.getFAQs()).resolves.toHaveLength(2);
-	});
+  it("should return the correct number of results with the seeded data", async () => {
+    const provider = new PayloadFAQProvider(PAYLOAD_ADDRESS);
+    await expect(provider.getFAQs()).resolves.toMatchInlineSnapshot(`
+      [
+        {
+          "answer": "How many?",
+          "question": "Third question?",
+        },
+        {
+          "answer": "Do you?",
+          "question": "I have a question?",
+        },
+      ]
+    `);
+  });
 });
