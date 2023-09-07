23128436da51019b53fe9bf1786d289ad89f70e7
Kian Musser <kianmusser@granderprogramming.com>
2023-09-07 14:46:46 -0500

    devops(cicd): parameterize version

--- run.earthly.on.docicd.sh
+++ run.earthly.on.docicd.sh
@@ -2,10 +2,12 @@
 
 # requires docker login on docicd
 
+sVersion="$1"
+
 docker run \
 	-t \
 	-v /var/run/docker.sock:/var/run/docker.sock \
 	-v /root/.docker:/root/.docker \
 	-e NO_BUILDKIT=1 \
 	-e EARTHLY_PUSH=1 \
-	earthly/earthly:v0.7.17 github.com/kian8017/indexingbrain+publish --PAYLOAD_ADDRESS=https://admin.indexing-brain.org --VERSION=test-dockerized-1
+	earthly/earthly:v0.7.17 github.com/kian8017/indexingbrain+publish --PAYLOAD_ADDRESS=https://admin.indexing-brain.org --VERSION="$sVersion"
