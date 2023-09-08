b239a1423199f67b96cd9993ff2375f303eb5c2f
Kian Musser <kianmusser@granderprogramming.com>
2023-09-08 13:58:57 -0500

    devops(earthly): figure out where --push flag goes

--- run.earthly.on.docicd.sh
+++ run.earthly.on.docicd.sh
@@ -9,5 +9,4 @@ docker run \
 	-v /var/run/docker.sock:/var/run/docker.sock \
 	-v /root/.docker:/root/.docker \
 	-e NO_BUILDKIT=1 \
-	-e EARTHLY_PUSH=1 \
-	earthly/earthly:v0.7.17 github.com/kian8017/indexingbrain+publish --PAYLOAD_ADDRESS=https://admin.indexing-brain.org --VERSION="$sVersion"
+	earthly/earthly:v0.7.17 --push github.com/kian8017/indexingbrain+publish --PAYLOAD_ADDRESS=https://admin.indexing-brain.org --VERSION="$sVersion"
